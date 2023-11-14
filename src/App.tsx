1e09ed8ffe57332a6ad72e9708b9889bcdda0cb1
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-14 17:32:41 +0530

    Add new components and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@chakra-ui/react";
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <Button colorScheme="blue">Button</Button>
+    <div className=" grid grid-cols-2">
+      <div className="bg-green-300 col-span-2">Nav</div>
+      <div className=" bg-red-400 max-lg:hidden">Aside</div>
+      <div className=" bg-blue-200 max-lg:col-span-2">Main</div>
     </div>
   );
 }
