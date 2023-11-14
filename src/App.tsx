da5f1bf7b65f3af0f77faf018fede24a60d97598
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-14 17:14:22 +0530

    Add Chakra UI button and dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@chakra-ui/react";
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
 }
 
 export default App;
