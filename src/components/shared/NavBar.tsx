cf1826eeedd331c39d1bf44970b0811db6b35737
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-14 17:53:00 +0530

    Add theme toggle to navbar and dropdown menu component

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,10 +1,14 @@
 import logo from "../../assets/logo.webp";
+import ModeToggle from "../theme/ModeToggle";
 
 const NavBar = () => {
   return (
-    <nav className=" flex items-center gap-3">
-      <img src={logo} height={60} width={60} alt="logo" />
-      <span>Navbar</span>
+    <nav className=" flex items-center justify-between px-5 py-3">
+      <div className=" flex items-center">
+        <img src={logo} height={60} width={60} alt="logo" />
+        <span>Navbar</span>
+      </div>
+      <ModeToggle />
     </nav>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+import { ThemeProvider } from "@/components/theme/theme-provider.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
