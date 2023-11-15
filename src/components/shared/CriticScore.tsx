b1880aa1afab2816f20cfc49528206f9fb0952dc
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-15 19:01:50 +0530

    Add CriticScore component to GameCard and update useGames hook to include metacritic score

---
+++ src/components/shared/CriticScore.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "../ui/badge";
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  return (
+    <Badge
+      className={`${
+        score > 75
+          ? "text-green-800 bg-green-100"
+          : "bg-yellow-100 text-yellow-600"
+      }  rounded-sm `}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
--- src/components/shared/GameCard.tsx
+++ src/components/shared/GameCard.tsx
@@ -1,25 +1,26 @@
 import { Game } from "@/hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <div
-      className="flex flex-col gap-5 justify-start shadow dark:bg-gray-900  rounded-lg overflow-hidden pb-8 hover:scale-105  
-    duration-500 hover:-translate-y-2 cursor-pointer hover:shadow-md dark:shadow-sky-400 dark:hover:drop-shadow-md"
-    >
-      <div className="w-full flex justify-center items-start overflow-hidden h-36 ">
+    <div className="flex flex-col justify-start gap-5 pb-8 overflow-hidden duration-500 rounded-lg shadow cursor-pointer dark:bg-gray-900 hover:scale-105 hover:-translate-y-2 hover:shadow-md dark:shadow-sky-400 dark:hover:drop-shadow-md">
+      <div className="flex items-start justify-center w-full overflow-hidden h-36 ">
         <img src={game.background_image} />
       </div>
       <h2 className="px-5 text-xl font-bold dark:text-slate-200 text-slate-700 line-clamp-1">
         {game.name}
       </h2>
-      <div className=" flex gap-2 px-5 flex-wrap text-slate-500">
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+      <div className="flex items-center justify-between pr-3">
+        <div className="flex flex-wrap gap-2 px-5 text-slate-500">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+        </div>
+        <CriticScore score={game.metacritic} />
       </div>
     </div>
   );
