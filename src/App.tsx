43214ed99c340cd617be0a2534f9c52de388ad80
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-17 19:54:59 +0530

    Refactor game query state in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,14 @@ import { Genre } from "./hooks/useGenres";
 import PlatformList from "./components/shared/PlatformList";
 import { Platform } from "./hooks/useGames";
 
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
+
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const [selectedPlatform, setSelctedPlatform] = useState<Platform | null>(
-    null
-  );
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
   return (
     <div className="mx-auto max-w-screen-[1800px]">
       <div className="w-full ">
@@ -19,18 +22,17 @@ function App() {
       <div className="flex ">
         <div className="w-[350px]  max-lg:hidden bg-secondary/40 pt-10 pl-5  shadow-lg">
           <GenreList
-            selectedGenre={selectedGenre}
-            onSelectedGenre={(genre) => setSelectedGenre(genre)}
+            selectedGenre={gameQuery.genre}
+            onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
           />
         </div>
         <div className="flex flex-col w-full gap-8 p-2 pt-10 bg-secondary/30">
           <PlatformList
-            onSelectPlatform={(platform) => setSelctedPlatform(platform)}
+            onSelectPlatform={(platform) =>
+              setGameQuery({ ...gameQuery, platform })
+            }
           />
-          <GameGrid
-            selectedGenre={selectedGenre}
-            selectedPlatform={selectedPlatform}
-          />
+          <GameGrid gameQuery={gameQuery} />
         </div>
       </div>
     </div>
