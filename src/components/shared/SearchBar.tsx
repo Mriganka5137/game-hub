fa699ba74fbe127951ae83e6a55e3ed6ff22cfca
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-11-24 10:05:56 +0530

    Add Search Input inside Form

--- src/components/shared/SearchBar.tsx
+++ src/components/shared/SearchBar.tsx
@@ -1,12 +1,26 @@
+import { useRef } from "react";
 import { Input } from "../ui/input";
 
 const SearchBar = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <Input
-      type="search"
-      placeholder="Search..."
-      className="max-w-screen-lg rounded-full bg-secondary/30"
-    />
+    <form
+      className="w-full "
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (inputRef.current) {
+          console.log(inputRef.current.value);
+        }
+      }}
+    >
+      <Input
+        type="search"
+        placeholder="Search..."
+        className="max-w-screen-lg mx-auto rounded-full bg-secondary/30"
+        ref={inputRef}
+      />
+    </form>
   );
 };
 
