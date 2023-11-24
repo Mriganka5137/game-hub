8f2819d1a98876d9baa8a6076b928b8cdaab8bb3
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-11-24 09:23:43 +0530

    Add new images and update image URL handling

--- src/utils/image-url.ts
+++ src/utils/image-url.ts
@@ -1,5 +1,6 @@
+import placeHolder from "../assets/GameHub Resources/Image Placeholder/no-image-placeholder-6f3882e0.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeHolder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
