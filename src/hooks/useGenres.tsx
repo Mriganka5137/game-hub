a95be2b2c5ef8ba869b64a0c425f64b8e775f386
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-07 07:16:43 +0530

    Fix useGenres queryFn bug

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -12,7 +12,7 @@ export interface Genre {
 const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
-    queryFn: () => apiClient.getAll,
+    queryFn: apiClient.getAll,
 
     staleTime: 24 * 60 * 60 * 1000, //24 hrs,
   });
