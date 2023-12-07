1696e1bd6f847e28a53386d58053857845f57f59
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-07 08:21:16 +0530

    Implement Infinite Query

--- src/components/shared/GameGrid.tsx
+++ src/components/shared/GameGrid.tsx
@@ -2,13 +2,20 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import useGames from "@/hooks/useGames";
 import { GameQuery } from "@/App";
-
+import React from "react";
+import { Button } from "../ui/button";
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading: loading } = useGames(gameQuery);
+  const {
+    data,
+    error,
+    isLoading: loading,
+    fetchNextPage,
+    isFetching,
+  } = useGames(gameQuery);
   const skeletonArr = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
@@ -23,10 +30,25 @@ const GameGrid = ({ gameQuery }: Props) => {
       ) : error ? (
         <p>{error.message}</p>
       ) : (
-        <div className="flex flex-wrap justify-center px-5 gap-9">
-          {data?.results.map((game) => (
-            <GameCard game={game} key={game.id} />
-          ))}
+        <div className="flex flex-col justify-start">
+          <div className="flex flex-wrap justify-center px-5 gap-9">
+            {data?.pages.map((page, index) => (
+              <React.Fragment key={index}>
+                {page.results.map((game) => (
+                  <GameCard game={game} key={game.id} />
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+          <div className="flex items-center justify-center mt-10 ">
+            <Button
+              className=" w-fit"
+              variant="default"
+              onClick={() => fetchNextPage()}
+            >
+              {isFetching ? "Loading..." : "Load More"}
+            </Button>
+          </div>
         </div>
       )}
     </>
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,6 +1,6 @@
 import { GameQuery } from "@/App";
-import { useQuery } from "@tanstack/react-query";
-import APIClient, { Platform } from "@/services/api-client";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import APIClient, { FetchResponse, Platform } from "@/services/api-client";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -14,17 +14,22 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: (pageParam) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOptions,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1,
   });
 
 export default useGames;
